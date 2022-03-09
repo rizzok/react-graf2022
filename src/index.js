c561a34aa942fe4f9d18d0df38efebee4339270a
rizzok <kevinrizzo13100@gmail.com>
2022-03-09 19:14:43 +0100

    new Parcel project

--- src/index.js
+++ src/index.js
@@ -1,17 +1,20 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React, { useState } from 'react';
+import ReactDOM from "react-dom";
+
+function Compteur() {
+    const [count, setCount] = useState(0)
+
+    const handleClick = function (e) {
+        e.preventDefault() 
+        setCount(10)
+    }
+    
+    return <button onClick={handleClick}>Nombre : {count}</button>
+}
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+    <div>
+        <Compteur/>
+    </div>,
+    document.getElementById("app")
+)
