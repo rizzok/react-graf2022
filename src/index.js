701c70068b19859be3cc442dad9bc9512cc057bb
rizzok <kevinrizzo13100@gmail.com>
2022-03-10 20:39:29 +0100

    initialize parcel project

--- src/index.js
+++ src/index.js
@@ -1,48 +1,15 @@
-import React, { useState, useEffect, useCallback } from 'react';
+import React, { useState, useEffect } from 'react';
 import ReactDOM from "react-dom";
 
-function PrimaryButton({children}) {
-    return (
-        <button type="submit">{children}</button>
-    )
-}
-
-function FormField(props) {
+function App() {
     return (
         <div>
-            <label htmlFor={props.name}>{props.children} : </label>
-            <input type="text" name={props.name} id={props.name} />
+            <h1>Sandbox</h1>
         </div>
     )
-}
-
-function FormContext({children}) {
-    return (
-        <form>
-            {children}
-        </form>
-    )
-}
-
-function App() {
-
-    const handleSubmit = useCallback((value) => {
-        console.log(value);
-    }, [])
-    
-    return (
-        <FormContext 
-            defaultValue={{name: 'Doe', firstname: 'John'}} 
-            onSubmit={handleSubmit}>
-                <FormField name="name">Nom</FormField>
-                <FormField name="firstname">Prénom</FormField>
-                <PrimaryButton>Envoyer</PrimaryButton>
-        </FormContext>
-    )
-
 }
 
 ReactDOM.render(
     <App />,
     document.getElementById("app")
-);
+)
