0ca40cc4f1d0ae85f2fa5b8cede0fec8b42ffa20
rizzok <kevinrizzo13100@gmail.com>
2022-03-11 10:35:17 +0100

    Update exercice

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
-import React, { useState, useEffect, useCallback } from 'react';
+import React, { useState, useEffect, useCallback, useContext, useMemo } from 'react';
 import ReactDOM from "react-dom";
+
+const FormContext = React.createContext({});
 
 function PrimaryButton({children}) {
     return (
@@ -7,20 +9,37 @@ function PrimaryButton({children}) {
     )
 }
 
-function FormField(props) {
+function FormField({name, children}) {
+    const data = useContext(FormContext)
+    const handleChange = useCallback(function(e) {
+        data.change(e.target.name, e.target.value)
+    }, [data.change])
+    
     return (
         <div>
-            <label htmlFor={props.name}>{props.children} : </label>
-            <input type="text" name={props.name} id={props.name} />
+            <label htmlFor={name}>{children} : </label>
+            <input type="text" name={name} id={name} 
+                value={data[name] || ''} onChange={handleChange} />
         </div>
     )
 }
 
-function FormContext({children}) {
+function FormWithContext({defaultValue, onSubmit, children}) {
+    const [data, setData] = useState(defaultValue)
+    const change = useCallback(function (name, value) {
+        setData(d => ({...d, [name]: value}))
+    })
+    const value = useMemo(function () {
+        return {...data, change}
+    }, [data, change])
+    
     return (
-        <form>
-            {children}
-        </form>
+        <FormContext.Provider value={value}>
+            <form onSubmit={onSubmit}>
+                {children}
+            </form>
+            {JSON.stringify(value)}
+        </FormContext.Provider>
     )
 }
 
@@ -31,13 +50,13 @@ function App() {
     }, [])
     
     return (
-        <FormContext 
+        <FormWithContext
             defaultValue={{name: 'Doe', firstname: 'John'}} 
             onSubmit={handleSubmit}>
-                <FormField name="name">Nom</FormField>
                 <FormField name="firstname">Prénom</FormField>
+                <FormField name="name">Nom</FormField>
                 <PrimaryButton>Envoyer</PrimaryButton>
-        </FormContext>
+        </FormWithContext>
     )
 
 }
